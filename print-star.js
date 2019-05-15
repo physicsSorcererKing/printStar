4fde9f44e1068322cf0e88c3093bbc3e08d5a8da
Shuo Wang <36099986+pilisenk@users.noreply.github.com>
2019-05-16 01:12:37 +0800

    Update print-star.js

--- print-star.js
+++ print-star.js
@@ -1,14 +1,13 @@
 const printStar = n => {
   if( n % 2 === 0 || n < 1 || !Number.isInteger(n) ) throw 'Error!'
-	let start = 0, end = n-1
-	for(let i = 0; i < n; ++i) {
-    		let string = ''
-		let l = start + i 
-		let r = end -i
-		for(let j = 0; j < n; ++j){
-			if( i <= n/2 ) string += j < l || j > r ? ' ' : '*'
-			else string += j < r || j > i ? ' ' : '*'
-		}
-		console.log(string)
-	}
+  const start = 0, end = n-1
+  for(let i = 0; i < n; ++i) {
+    let row = ''
+    let l = start + i 
+    let r = end -i
+    for(let j = 0; j < n; ++j){
+	row += ( i < n/2 ) ? ( j < l || j > r ? ' ' : '*') : ( j < r || j > i ? ' ' : '*' )
+    }
+    console.log(row)
+  }
 }
